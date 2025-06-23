2228240aa02eff206f962c7ac57239a469097e00
wouldyoueverfly <cpptypxe@ya.ru>
2025-06-23 22:03:19 +0300

    [Feature] items added to the tableview

---
+++ Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  TodoList
+//
+//  Created by Ruslan Khusainov on 23.06.2025.
+//
+
+import Foundation
+
+struct Todo {
+    let title: String
+    let isCompleted: Bool = false
+}
--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let todos = [
+        Todo(title: "Apply for iOS Dev job"),
+        Todo(title: "Buy PC and PS5 Pro"),
+        Todo(title: "Rent appartement"),
+        Todo(title: "Play games with friend")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let todo = todos[indexPath.row]
+        
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = todo.title
+        
+        return cell
+    }
+}
